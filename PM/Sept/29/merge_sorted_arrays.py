c2dae8a1a552b54a329cfe25771753f93e670c38
bottlemania <118786302+bottlemania@users.noreply.github.com>
2023-09-29 23:57:48 +0530

    Add files via upload

---
+++ PM/Sept/29/merge_sorted_arrays.py
@@ -0,0 +1,30 @@
+def merge_sorted_arrays(nums1, m, nums2, n):
+    # Initialize pointers for nums1 and nums2
+    i, j = m - 1, n - 1
+    # Initialize pointer for the end of nums1
+    k = m + n - 1
+
+    # Merge nums1 and nums2 from the end
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+
+    # If there are remaining elements in nums2, copy them to nums1
+    while j >= 0:
+        nums1[k] = nums2[j]
+        j -= 1
+        k -= 1
+
+
+nums1 = [1, 2, 3, 0, 0, 0]
+m = 3
+nums2 = [2, 5, 6]
+n = 3
+
+merge_sorted_arrays(nums1, m, nums2, n)
+print(nums1)  # Output: [1, 2, 2, 3, 5, 6]
