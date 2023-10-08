0a427499644434b1fc4755ef53dd92e9c7414048
manjunathBase2 <manjunathworkspace@gmail.com>
2023-10-08 11:57:44 +0530

    Day26

---
+++ PM/Oct/08 - Day26/timeDifference.py
@@ -0,0 +1,22 @@
+import datetime
+
+# Get the current time
+current_time = datetime.datetime.now()
+
+# Input the time you want to compare (in HH:MM:SS format)
+given_time_str = input("Enter the time (HH:MM:SS): ")
+
+try:
+    # Convert the given time string to a datetime object
+    given_time = datetime.datetime.strptime(given_time_str, "%H:%M:%S")
+    
+    # Calculate the time difference
+    time_difference = current_time - given_time
+    
+    # Extract hours, minutes, and seconds from the time difference
+    hours, remainder = divmod(time_difference.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    
+    print(f"Time difference: {hours} hours, {minutes} minutes, {seconds} seconds")
+except ValueError:
+    print("Invalid time format. Please use HH:MM:SS format.")
