27e9c6bcf788783741e1736a9a5c3908a90b70f9
manjunathBase2 <manjunathworkspace@gmail.com>
2023-10-09 23:18:58 +0530

    Day27

---
+++ PM/Oct/09 - Day27/reverseWord.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+        long int len=str.length(),temp=0;
+        for(int i=0;i<len/2;i++){
+            temp=str[len-i-1];
+            str[len-i-1]=str[i];
+            str[i]=temp;
+        }
+        return str;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+    	string s;
+    	cin >> s;
+    	Solution ob;
+    	cout << ob.reverseWord(s) << endl;
+	}
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
